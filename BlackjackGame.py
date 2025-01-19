f100217b6c06e48ecdb873c8ccabb35a9c04dc25
Victor Sunny <90870360+VictorSunny@users.noreply.github.com>
2025-01-19 13:32:24 +0100

    Update BlackjackGame.py

--- BlackjackGame.py
+++ BlackjackGame.py
@@ -5,17 +5,16 @@ repres = ["ace",2,3,4,5,6,7,8,9,10,"king","queen","jack"]
 
 random.shuffle(repres)
 
-new_arrangement = []
-for rep in repres:
-	new_arrangement.append(rep)
-
-k = str(new_arrangement[3])
 stake = ""
 player = []
 dealer = []
 n = 0
 x = []
 y = []
+
+new_arrangement = []
+for rep in repres:
+	new_arrangement.append(rep)
 
 def give():
 	global n
@@ -28,7 +27,8 @@ def give():
 	y.append(all[str(new_arrangement[-(n + 1)])])
 	
 	n += 1
-	
+n = 0
+		
 def give_only_player():
 	global n
 	global x
@@ -36,25 +36,45 @@ def give_only_player():
 	x.append(all[str(new_arrangement[n])])
 	print(f"your cards are: {player}")
 	n += 1
-		
-give()
-give()
-print(f"Your cards are: {player}")
-print(f"The dealer has 2 cards. One of which is {dealer[0]}")
+n = 0
 
-while stake != "s":	
-	stake = input("enter S to stand, enter any key to pick another card").lower()
-	if stake != "s":
-		give_only_player()
-	total_x = sum(x)
-	if total_x > 21:
-		for paper in player:
-			if paper == "ace":
-				total_x  -= 10
+stake = ""
+player = []
+dealer = []
+n = 0
+x = []
+y = []		
+def game():
+	global stake
+	random.shuffle(repres)
+	new_arrangement = []
+	for rep in repres:
+		new_arrangement.append(rep)
+	print(new_arrangement)
+	give()
+	give()
+	print(f"Your cards are: {player}")
+	print(f"The dealer has 2 cards. One of which is {dealer[0]}")
+	while stake != "s":	
+		stake = input("enter S to stand, enter any key to pick another card").lower()
+		if stake != "s":
+			give_only_player()
+		total_x = sum(x)
+		if total_x > 21:
+			for paper in player:
+				if paper == "ace":
+					total_x  -= 10
+	
+	if total_x == sum(y):
+		print("Draw")			
+	elif total_x > sum(y) and total_x <= 21:
+		print(f"Congregations! You win.\nYour cards are: {player}\nThe dealer's cards are {dealer}")
+	else:
+		print(f"You lost.\nYour cards are: {player}\nThe dealer's cards are {dealer}")
+	keep_playing = input("Would you like to play again? yes or no?	").lower()
+	if keep_playing == "yes":
+		game()
+	else:
+		print("Ended")
 
-if total_x == sum(y):
-	print("Draw")			
-elif total_x > sum(y) and total_x <= 21:
-	print(f"Congregations! You win.\nYour cards are: {player}\nThe dealer's cards are {dealer}")
-else:
-	print(f"You lost.\nYour cards are: {player}\nThe dealer's cards are {dealer}")
+game()
